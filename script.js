068b8728811ec6db84d37c347c874574defefaee
bhavanamb <mbbhavana9@gmail.com>
2024-05-01 16:35:07 -0500

    add start game

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ const removeJump = () => {
 
 const character = document.getElementById("character");
 document.addEventListener("click", jump);
+
+const block = document.getElementById("block");
+
+const gameOver = document.getElementById("game-over");
+
+const checkGameOver = () => {
+  const charactedTopValue = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+
+  const blockLeftValue = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+
+  if (charactedTopValue >= 130 && blockLeftValue < 40 && blockLeftValue > 0) {
+    //alert("game over");
+    document.getElementById("overlay").style.display = "block";
+    block.style.left = "40px";
+
+    block.style.animationPlayState = "paused";
+
+    character.style.animationPlayState = "paused";
+    // const gameOverText = document.createTextNode("Game Over");
+    // gameOver.appendChild(gameOverText);
+  }
+};
+
+setInterval(checkGameOver, 10);
+
+const startGame = () => {
+  const startBtn = document.getElementById("block");
+  startBtn.classList.add("slide-block");
+};
